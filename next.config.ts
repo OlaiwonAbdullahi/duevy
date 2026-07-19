273aab544ed07d320085d9884e1f6506ceffa2e8
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2026-07-19 15:32:51 +0100

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.duevy.app",
+      },
+      {
+        protocol: "https",
+        hostname: "duevy-be.onrender.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
